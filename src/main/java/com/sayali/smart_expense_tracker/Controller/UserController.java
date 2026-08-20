241777619ffade8cb33794568e9aafb174b0b44c
Sayali98-beep <sayalisunilmore@gmail.com>
2026-08-20 23:49:56 +0530

    user created succuessfully

--- src/main/java/com/sayali/smart_expense_tracker/Controller/UserController.java
+++ src/main/java/com/sayali/smart_expense_tracker/Controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import com.sayali.smart_expense_tracker.entity.User;
 import com.sayali.smart_expense_tracker.service.UserService;
 
