af1ae72ba692966e710142a020318465ae0a741c
Sayali98-beep <sayalisunilmore@gmail.com>
2026-09-13 13:12:20 +0530

    completed password hashing and Started with JWT

--- src/main/java/com/sayali/smart_expense_tracker/config/SecurityConfig.java
+++ src/main/java/com/sayali/smart_expense_tracker/config/SecurityConfig.java
@@ -3,15 +3,107 @@ package com.sayali.smart_expense_tracker.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.sayali.smart_expense_tracker.security.CustomUserDetailsService;
+import com.sayali.smart_expense_tracker.security.JwtAuthenticationFilter;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
+	private final CustomUserDetailsService userDetailsService;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthenticationFilter jwtAuthenticationFilter) {
+	
+		 this.userDetailsService = userDetailsService;
+		 this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+
+	
 	@Bean
 	PasswordEncoder passwordEncoder()
 	{
-		return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();		
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider()
+	{
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+		
+		provider.setPasswordEncoder(passwordEncoder());
+		
+		return provider;		
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager
+	             (AuthenticationConfiguration configuration) throws Exception
+	{
+		return configuration.getAuthenticationManager();
 		
 	}
+	
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(
+	        HttpSecurity http) throws Exception {
+
+	    http
+	        .csrf(csrf -> csrf.disable())
+
+	        .authorizeHttpRequests(auth -> auth
+
+	            .requestMatchers(
+	                "/login",
+	                "/register",
+	                "/users/new",
+	                "/users/save",
+	                "/users/forgot-password",
+	                "/users/forgot-password-form",
+	                "/css/**",
+	                "/js/**"
+	            ).permitAll()
+
+	            .anyRequest().authenticated()
+	        )
+
+	        .sessionManagement(session ->
+	            session.sessionCreationPolicy(
+	                SessionCreationPolicy.STATELESS
+	            )
+	        )
+
+	        .authenticationProvider(authenticationProvider())
+
+	        .addFilterBefore(
+	            jwtAuthenticationFilter,
+	            UsernamePasswordAuthenticationFilter.class
+	        );
+
+	    return http.build();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
---
+++ src/main/java/com/sayali/smart_expense_tracker/security/JwtService.java
@@ -0,0 +1,69 @@
+package com.sayali.smart_expense_tracker.security;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    private SecretKey getSigningKey() {
+
+        return Keys.hmacShaKeyFor(
+                secretKey.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken(UserDetails userDetails) {
+
+        return Jwts.builder()
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(
+            String token,
+            UserDetails userDetails) {
+
+        String username = extractUsername(token);
+
+        return username.equals(userDetails.getUsername())
+                && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        Date expiration = Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration();
+
+        return expiration.before(new Date());
+    }
+}
